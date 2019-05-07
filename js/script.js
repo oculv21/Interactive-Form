965db8a3daf43655d01e2640e53ee3df22586bc5
Olivia Culver <oculv21@gmail.com>
2019-05-07 05:27:38 -0400

    added other job role field functionality in Job Role select

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+//first input field in focus
+$('#name').focus();
+
+//"Other" Job Role input field
+$('#other-title').hide();
+$('#title').change(function () {
+  if ( $(this).val() == 'other' ) {
+    $('#other-title').show();
+  } else {
+    $('#other-title').hide();
+  };
+});
+
+//Assign T-Shirt colors to theme
+const punsTshirt = $('#design option[value="js puns"]');
+const heartTshirt = $('#design option[value="heart js"]');
+
+
+
