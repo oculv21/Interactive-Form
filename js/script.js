10691ebc57486d4533c0b7a6c5321909e411c809
Olivia Culver <oculv21@gmail.com>
2019-05-08 16:54:57 -0400

    added functionality to tshirt info section

--- js/script.js
+++ js/script.js
@@ -2,24 +2,29 @@
 $('#name').focus();
 
 //"Other" Job Role input field
-$('#other-title').hide();
+$('#other-input').hide();
 
 $('#title').change(function () {
   if ( $(this).val() == 'other' ) {
-    $('#other-title').show();
+    $('#other-input').show();
   } else {
-    $('#other-title').hide();
+    $('#other-input').hide();
   };
 });
 
 //Assign T-Shirt colors to theme
+$('#design option[value="theme"]').prop('hidden', true)
 $('#design').change(function () {
     if ( $(this).val() == 'js puns') {
-        $('#color').find('.heart').toggle(false);
-        $('#color').find('.puns').toggle(true);
+        $('#color').find('.heart').hide();
+        $('#color').find('.puns').show();
+        $('#color option[value="cornflowerblue"]').prop('selected', true);
+        $('#color option[value="tomato"]').prop('selected', false);
     } else {
         $('#color').find('.puns').toggle(false);
         $('#color').find('.heart').toggle(true);
+        $('#color option[value="tomato"]').prop('selected', true);
+        $('#color option[value="cornflowerblue"]').prop('selected', false);
     };
 });
 
